e3fe20a8907ddb7930dab06dc676ab8259c98b59
vinicius <vnslongo@gmail.com>
2022-10-14 22:24:42 -0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+//o mod nomeado main é o target da execução cargo run
+
+mod test;
+
+use test::variable::execute;
+
+fn main() {
+    execute();
+}
---
+++ src/test/mod.rs
@@ -0,0 +1,10 @@
+// por padrao as funções declaras sao privadas
+// pub - torna a função declarada, pública
+
+
+// execute cmd targets mod.rs inside any folder
+// test (folder)
+// |
+//  --- > mod.rs <- execute this
+
+pub mod variable;
